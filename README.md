bc45621363e253b4d9ad62c0bb4d4bb87b989b8e
Casey Kneale <ckneale@udel.com>
2020-11-01 14:23:05 -0500

    init commit

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
-# ExtremeVertexDesigns
+# ExtremeVertexDesigns.jl
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://caseykneale.github.io/ExtremeVertexDesigns.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://caseykneale.github.io/ExtremeVertexDesigns.jl/dev)
 [![Build Status](https://github.com/caseykneale/ExtremeVertexDesigns.jl/workflows/CI/badge.svg)](https://github.com/caseykneale/ExtremeVertexDesigns.jl/actions)
 [![Coverage](https://codecov.io/gh/caseykneale/ExtremeVertexDesigns.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/caseykneale/ExtremeVertexDesigns.jl)
+
+## Status
+Work in progress.
+
+## Overview
+Package for generating extreme value design candidate sets from inequality constraints. These types of experimental designs are useful for mixture experiments and generating calibrations.
+
+## Example
+
+```Julia
+using ExtremeVertexDesigns
+using JuMP
+
+model = Model()
+
+@variables(model, begin
+    x
+    y
+end)
+
+@constraints(model, begin
+    0 <= x <= 1.0
+    0 <= y <= 1.0
+    x <= y
+end)
+
+evs = extreme_values( model; barycenters = true)
+
+federov_D( 4, evs; k = 1e-6 )
+```
+
+## Credits
+Thank you to Júlio Hoffimann, and Benoît Legat for helping me grok Polyhedra.jl.
--- src/ExtremeVertexDesigns.jl
+++ src/ExtremeVertexDesigns.jl
@@ -1,5 +1,74 @@
 module ExtremeVertexDesigns
 
-# Write your package code here.
+    using Polyhedra, CDDLib, JuMP, Statistics, StatsBase, LinearAlgebra
+
+    """
+    extreme_values( model::JuMP.Model; barycenters = true)::Matrix{Float64}
+
+        From an inequality constrained JuMP model, return a matrix containing candidate 
+        design points.
+        Note: Only barycenters of faces are currently supported. Soon, interior barycenters will also be
+        added.
+    """
+    function extreme_values( model::JuMP.Model; barycenters = true)::Matrix{Float64}
+        #ToDo: Invite other backends?
+        poly = polyhedron(model, CDDLib.Library())
+        removehredundancy!(poly)
+        #ToDo: Faster memcpy then vcat from pointer?
+        candidates = transpose(hcat(points(poly)...))
+        if barycenters
+            barycenters = [ mean( incidentpoints(poly, hidx) ) for hidx in eachindex(halfspaces(poly))]
+            #ToDo: Faster memcpy then vcat from pointer?
+            barycenters = transpose( hcat( barycenters...) )
+            grand_center = mean( barycenters, dims = 1 )
+            candidates = vcat( candidates, barycenters, grand_center )
+        end
+        return candidates
+    end
+    export extreme_values
+
+    """
+    federov_D(Ndesign::Int, candidates::Matrix; k = 1e-6)
+
+        Performs Federov exchange algorithm to select a D-optimal design from a candidate matrix.
+        Note: k is a ridge parameter to invert a matrix. This will be swapped out for an
+        invertible starting design algorithm later...
+    """
+    function federov_D(Ndesign::Int, candidates::Matrix; k = 1e-6)
+        Nsupport, V = size(candidates)
+        @assert Ndesign < Nsupport "Design points must be < than support candidates."
+        init_samples = sample(1:Nsupport, Ndesign; replace = false)
+        Xdesign = candidates[init_samples,:]
+        Xsupport = candidates[ setdiff(1:Nsupport, init_samples),:]
+        Nsupport -= Ndesign 
+        #ToDo: implement invertible starting design algorithm.
+        tihkinov = Diagonal(ones(V) .* k)
+        
+        while(true) 
+            XdtXd       = inv( Xdesign' * Xdesign + tihkinov )
+            σ_design    = argmax( [ r' * XdtXd * r for r in eachrow( Xdesign ) ] )
+            σ_support   = argmax( [ r' * XdtXd * r for r in eachrow( Xsupport ) ] )
+            biggest_ds  = -Inf
+            pair        = (0,0)
+            for nd in 1:Ndesign, ns in 1:Nsupport
+                σ_ds    = Xdesign[nd,:]' * XdtXd * Xsupport[ns,:]
+                ds      = σ_support - ((σ_design * σ_support) - (σ_ds ^ 2) ) - σ_design
+                if ds > biggest_ds
+                    biggest_ds = ds
+                    pair    = (nd, ns)
+                end
+            end
+            if biggest_ds <= 0
+                break
+            end
+            tmp = Xdesign[first(pair),:]
+            Xdesign[first(pair),:] = Xsupport[last(pair),:]
+            Xsupport[last(pair),:] = tmp
+        end
+        return (    score = det(inv( Xdesign' * Xdesign + tihkinov )), 
+                    design = Xdesign )
+    end
+    export federov_D
 
 end
+
